118344c02b46b2310a35d3733b881c087195e087
Lee-Yu-Ying0426 <152320007+Lee-Yu-Ying0426@users.noreply.github.com>
2025-06-16 18:53:10 +0800

    Day1_HelloCSharp

--- Day1_HelloCSharp/obj/Debug/net6.0/ConsoleApp1.AssemblyInfo.cs
+++ Day1_HelloCSharp/obj/Debug/net6.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1167e094bd0b195cb89d1217ead27ba0ea61f938")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
