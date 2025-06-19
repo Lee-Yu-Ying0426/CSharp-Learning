5ecc1a165fbf8d6442e33e53643c2e23fdc63229
Lee-Yu-Ying0426 <152320007+Lee-Yu-Ying0426@users.noreply.github.com>
2025-06-19 20:13:10 +0800

    Day6_多帳號登入驗證
    字典的使用

---
+++ Day6_MultiUserLogin/Day6_MultiUserLogin/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Day6_MultiUserLogin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.userName = new System.Windows.Forms.TextBox();
+            this.userPassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.login = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 32F);
+            this.label1.Location = new System.Drawing.Point(24, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 43);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "帳號:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // userName
+            // 
+            this.userName.Font = new System.Drawing.Font("新細明體", 32F);
+            this.userName.Location = new System.Drawing.Point(146, 34);
+            this.userName.Name = "userName";
+            this.userName.Size = new System.Drawing.Size(250, 59);
+            this.userName.TabIndex = 1;
+            this.userName.TextChanged += new System.EventHandler(this.userName_TextChanged);
+            // 
+            // userPassword
+            // 
+            this.userPassword.Font = new System.Drawing.Font("新細明體", 32F);
+            this.userPassword.Location = new System.Drawing.Point(146, 143);
+            this.userPassword.Name = "userPassword";
+            this.userPassword.Size = new System.Drawing.Size(250, 59);
+            this.userPassword.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 32F);
+            this.label2.Location = new System.Drawing.Point(24, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 43);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "密碼:";
+            // 
+            // login
+            // 
+            this.login.Font = new System.Drawing.Font("新細明體", 32F);
+            this.login.Location = new System.Drawing.Point(146, 243);
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(250, 67);
+            this.login.TabIndex = 4;
+            this.login.Text = "登入";
+            this.login.UseVisualStyleBackColor = true;
+            this.login.Click += new System.EventHandler(this.login_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("新細明體", 32F);
+            this.lblResult.Location = new System.Drawing.Point(471, 67);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 43);
+            this.lblResult.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1079, 489);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.login);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.userPassword);
+            this.Controls.Add(this.userName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox userName;
+        private System.Windows.Forms.TextBox userPassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button login;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ Day6_MultiUserLogin/Day6_MultiUserLogin/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Day6_MultiUserLogin
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, string> accounts = new Dictionary<string, string>(); // 帳號 ➜ 密碼
+        Dictionary<string, string> profiles = new Dictionary<string, string>(); // 帳號 ➜ 使用者名稱
+        Dictionary<string, int> loginAttempts = new Dictionary<string, int>(); // 帳號➜ 錯誤次數
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            accounts.Add("admin", "1234");
+            accounts.Add("guest", "0000");
+
+            profiles.Add("admin", "Yu-Ying");
+            profiles.Add("guest", "Visitor");
+
+            loginAttempts.Add("admin", 0);
+            loginAttempts.Add("guest", 0);
+
+        }
+
+        private void userName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void login_Click(object sender, EventArgs e)
+        {
+            string inputAccount = userName.Text;
+
+            if (accounts.ContainsKey(userName.Text))  //是否存在帳號
+            {
+                if (loginAttempts.ContainsKey(inputAccount) && loginAttempts[inputAccount] <= 1)
+                {
+                    if (userPassword.Text == accounts[inputAccount]) //密碼是否正確
+                    {
+                        loginAttempts[inputAccount] = 0;
+                        string displayName = profiles[inputAccount];
+                        lblResult.Text = $"歡迎使用者：{displayName}。";
+                    }
+                    else
+                    {
+                       int log = loginAttempts[inputAccount] + 1;
+                        loginAttempts[inputAccount] += 1;
+                        int sum = 3 - log;
+                        lblResult.Text = $"密碼錯誤，請再試一次。\n剩餘次數{sum}";
+                    }
+                }
+                else
+                {
+                    lblResult.Text = "密碼錯誤已達 3 次。\n帳號已鎖定。";
+                }
+            }         
+        }
+    }
+}
